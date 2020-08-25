01ef24c558e2045af2481a3f0af64f9a61e8e8a2
远山 周 <1732559983@qq.com>
2020-08-25 14:10:30 +0800

    登录状态存储

--- src/main/java/edu/dwlx/config/WebSecurityConfig.java
+++ src/main/java/edu/dwlx/config/WebSecurityConfig.java
@@ -1,11 +1,16 @@
 package edu.dwlx.config;
 
 import edu.dwlx.services.UserService;
+import org.checkerframework.checker.units.qual.A;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 
 @Configuration
 @EnableWebSecurity
@@ -23,18 +28,21 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 //                .roles("USER");
 //        auth.userDetailsService(new UserService());
 //    }
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
                     .antMatchers("/zhifou/user/register", "/zhifou/user/login",
-                            "/zhifou/user/register.html", "/css/**","/img/**","/js/**").permitAll()
+                            "/zhifou/user/register.html", "/css/**","/img/**","/js/**",
+                            "/login", "/").permitAll()
+                    .antMatchers("/zhifou/people/**", "/zhifou/know/**").hasAuthority("USER")
                     .anyRequest().authenticated()
                     .and()
                 .formLogin()
                     .loginPage("/zhifou/user/login.html")
                     .loginProcessingUrl("/zhifou/user/login")
+                    .usernameParameter("username")
+                    .passwordParameter("password")
 //                    .defaultSuccessUrl("/zhifou/know/index.html")
                     .permitAll()
                     .and()
@@ -42,18 +50,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                     .permitAll()
                     .logoutUrl("/zhifou/user/logout")
                     .logoutSuccessUrl("/zhifou/user/login.html")
-//                .logoutSuccessHandler(new LogoutSuccessHandler() {
-//                    @Override
-//                    public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-//
-//                    }
-//                })
-//                .addLogoutHandler(new LogoutHandler() {
-//                    @Override
-//                    public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
-//
-//                    }
-//                })
                     .invalidateHttpSession(true);
         http.csrf().disable();
 //                .deleteCookies("token_token")
