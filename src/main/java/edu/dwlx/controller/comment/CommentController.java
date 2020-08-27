45fe8263908b8c4d3466ee10aeaed766d01a4712
远山 周 <1732559983@qq.com>
2020-08-27 10:51:18 +0800

    个人收藏修改
    查看个人收藏时会返回map数据类型, 里面存放两个键值对, 一个键是answers代表收藏的回答列表, 对应值是List<Answer>. 另一个键是answers, 值是收藏的文章列表

--- src/main/java/edu/dwlx/controller/comment/CommentController.java
+++ src/main/java/edu/dwlx/controller/comment/CommentController.java
@@ -1,16 +1,22 @@
 package edu.dwlx.controller.comment;
 
 import edu.dwlx.entity.Comment;
+import edu.dwlx.mapper.CommentMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/zhifou/comment")
 public class CommentController {
 
-//    @Autowired
+    @Autowired
+    CommentMapper commentMapper;
 //    @RequestMapping
-//    public List<Comment>
+//    public List<Comment> getCommentList(String comment) {
+//        return commentMapper.
+//    }
 
 }
