29bf75e466b55d10a9410d764deac621615b08ed
zsw-2281719089 <69949220+zsw-2281719089@users.noreply.github.com>
2020-08-27 11:23:55 +0800

    删除question列并添加昵称

--- src/main/java/edu/dwlx/mapper/UserMapper.java
+++ src/main/java/edu/dwlx/mapper/UserMapper.java
@@ -22,8 +22,6 @@ public interface UserMapper {
 
     void createCollectAnswerTable(User user);
 
-    void createUserQuestionTable(User user);
-
     void createUserAnswerTable(User user);
 
     int updateUser(User user);
