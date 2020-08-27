d394b6fa94b9d5d0a003f0f0f39984fc2e37e7ff
zsw-2281719089 <69949220+zsw-2281719089@users.noreply.github.com>
2020-08-27 10:48:39 +0800

    8.27:10.50

--- src/main/java/edu/dwlx/mapper/CommentMapper.java
+++ src/main/java/edu/dwlx/mapper/CommentMapper.java
@@ -1,7 +1,18 @@
 package edu.dwlx.mapper;
 
+import edu.dwlx.entity.Comment;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 @Mapper
 public interface CommentMapper {
+    void insertComment(@Param("comment")Comment comment, @Param("tableName")String tableName);
+
+    void deleteComment(@Param("comment")Comment comment, @Param("tableName")String tableName);
+
+    List<Comment> searchCommentByTableName(@Param("tableName") String tableName);
+
+    void updateComment(@Param("comment")Comment comment, @Param("tableName")String tableName);
 }
