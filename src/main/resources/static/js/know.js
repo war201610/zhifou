bbaa781e25b1597b312979426f9033c9e855020c
chk <1587837621@qq.com>
2020-08-28 09:24:18 +0800

    完成推荐页提出问题
    完成基础功能，其他待优化，继续做问题页的回答。

---
+++ src/main/resources/static/js/know.js
@@ -0,0 +1,43 @@
+// 获取session中的uid
+const uid = $(".session-user").val()
+
+// 获取提问标签
+function getTag() {
+    const allTag = document.getElementsByName("gridCheck1")
+    var i = 0;
+    var tagArray
+    for (var j=0; j<allTag.length; j++) {
+        if(allTag[j].checked) {
+            tagArray[i++] = allTag[j].value
+        }
+    }
+    return tagArray
+}
+
+// 点击事件-提交问题
+$("#btn-confirm-put").click(function () {
+    const content = $("#exampleFormControlTextarea1").val()
+    const introduction = $("#exampleFormControlTextarea2").val()
+    var tagArray = getTag()
+    const ask = {
+        "uid": uid,
+        "content": content,
+        "introduction": introduction,
+        "tag": tagArray
+    }
+    $.post("/zhifou/ask", ask, function (result) {
+        console.log("发出提问请求：", result)
+    })
+})
+
+
+$(document).ready(function () {
+    // const Alltag = console.log(document.getElementsByName("gridCheck1"));
+    const Alltag = document.getElementsByName("gridCheck1")
+    for (var j = 0; j < Alltag.length; j++) {
+        var ck = Alltag[j];
+        if (ck.checked) {
+            console.log(ck.value)
+        }
+    }
+})
