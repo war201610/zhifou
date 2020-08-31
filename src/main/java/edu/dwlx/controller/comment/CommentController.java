458469d29e39b6217fff6086ff674fe458e342a3
远山 周 <1732559983@qq.com>
2020-08-31 16:00:19 +0800

    小修改

--- src/main/java/edu/dwlx/controller/comment/CommentController.java
+++ src/main/java/edu/dwlx/controller/comment/CommentController.java
@@ -21,6 +21,15 @@ public class CommentController {
 请求方式：get
 请求地址：zhifou/comment/评论表名/number
 请求数据：评论表内的评论条数。
+3. 发表评论(分问题, 文章, 回答)
+请求url: /zhifou/comment/question/问题id/add
+/zhifou/comment/article/文章id/add
+/zhifou/comment/answer/回答id/add
+请求方式: post
+请求参数: int uid; 用户id
+         int towho; 评论是回答谁的, 如果是评论文章, 问题或回答的就	                                         		         填-1, 如果是回复另一个人评论的就填用户id
+         String comment; 评论内容
+返回参数: Comment comment; 从数据库中查询的评论
 */
     @Autowired
     CommentMapper commentMapper;
--- src/main/java/edu/dwlx/controller/question/QuestionController.java
+++ src/main/java/edu/dwlx/controller/question/QuestionController.java
@@ -14,6 +14,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/zhifou/question")
