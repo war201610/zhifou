9c439121a235892a8b54a61dddbe133735031dd2
chk <1587837621@qq.com>
2020-08-31 11:31:12 +0800

    front-getUser

--- src/main/resources/static/js/know.js
+++ src/main/resources/static/js/know.js
@@ -30,10 +30,25 @@ $("#btn-confirm-put").click(function () {
     })
 })
 
+// 获取推荐（首页）列表
+function getKnowList() {
+    $.get("/zhifou/know/info", function (resultList) {
+        console.log("推荐页数据：", resultList)
+    })
+}
+
+function getUser() {
+    $.get("/getUser", function (resultList) {
+        console.log("推荐页数据：", resultList)
+    })
+}
+
 
 $(document).ready(function () {
     console.log($(".session-user").val())
     uid = $(".session-user").val()
     console.log("给uid赋值know:", $(".session-user").val())
     // console.log(localStorage.getItem("u"));
+    getKnowList()
+    getUser()
 })
