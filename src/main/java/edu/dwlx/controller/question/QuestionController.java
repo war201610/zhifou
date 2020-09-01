91f63688d6a0741a7007a3cfac2c31a69bc28c0a
zsw-2281719089 <69949220+zsw-2281719089@users.noreply.github.com>
2020-09-01 10:00:48 +0800

    问题的回答列表

--- src/main/java/edu/dwlx/controller/question/QuestionController.java
+++ src/main/java/edu/dwlx/controller/question/QuestionController.java
@@ -1,6 +1,8 @@
 package edu.dwlx.controller.question;
 
+import edu.dwlx.entity.Answer;
 import edu.dwlx.entity.Question;
+import edu.dwlx.services.AnswerService;
 import edu.dwlx.services.QuestionService;
 import edu.dwlx.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -22,6 +25,7 @@ public class QuestionController {
 
     @Autowired
     QuestionService questionService;
+    AnswerService answerService;
 
     //问题页
     @RequestMapping("/{id}")
@@ -42,5 +46,20 @@ public class QuestionController {
         //没指定跳转的页面
         request.getRequestDispatcher("/zhifou/question/answer.html");
         return new ArrayList<>();
+    }
+
+    @RequestMapping("/{questionId}/answer/{answerId}")
+    @ResponseBody
+    public Map<String, Object> getCollectAnswer(@PathVariable("questionId")int questionId, @PathVariable("answerId")int answerId){
+        Map<String, Object> map = new HashMap<String, Object>();
+        Question question = questionService.searchQuestionById(questionId);
+        map.put("question", question);
+        List<Answer> list = answerService.searchAnswerByQuestionId(questionId);
+        Answer answer1 = list.get(answerId);
+        Answer answer2 = list.get(0);
+        list.set(0, answer1);
+        list.set(answerId, answer2);
+        map.put("list", list);
+        return map;
     }
 }
