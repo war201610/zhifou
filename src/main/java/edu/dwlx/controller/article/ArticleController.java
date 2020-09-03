c3612927507aec42388d80be994520f10436273a
远山 周 <1732559983@qq.com>
2020-09-03 09:25:40 +0800

    as

--- src/main/java/edu/dwlx/controller/article/ArticleController.java
+++ src/main/java/edu/dwlx/controller/article/ArticleController.java
@@ -51,18 +51,17 @@ public class ArticleController {
     }
 
     @RequestMapping("/save")
-    @ResponseBody
-    public int save(Article article, String[] checkbox){
+//    @ResponseBody
+    public String save(Article article, String[] checkbox){
         String tag = "";
         for(String s: checkbox)
             tag += "#" + s;
         article.setTag(tag);
         if(article.getId() == 0){
             articleService.insertArticle(article);
-            return 0;
         }else{
             articleService.updateArticle(article);
-            return 1;
         }
+        return "/zhifou/know/know.html";
     }
 }
